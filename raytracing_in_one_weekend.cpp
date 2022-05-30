92b400e5350655323dfeb567d86600096301e49e
WhoLeb <gleb_lyah@mail.ru>
2022-05-30 11:00:45 +0300

    I DID THE MULTITHREADING. The code in release works much faster now. Also added the benchmarking.

--- raytracing_in_one_weekend.cpp
+++ raytracing_in_one_weekend.cpp
@@ -5,122 +5,218 @@
 #include "material.h"
 #include "sphere.h"
 
+#include <chrono>
 #include <iostream>
+#include <thread>
 
-color ray_color(const ray& r, const hittable& world, int depth) 
+color ray_color(const ray& r, const hittable& world, int depth)
 {
-    hit_record rec;
+	hit_record rec;
 
-    if (depth <= 0)
-        return color(0, 0, 0);
+	if (depth <= 0)
+		return color(0, 0, 0);
 
-    if (world.hit(r, 0.001, infinity, rec))
-    {
-        //point3 target = rec.p + rec.normal + random_unit_vector();
-        //point3 target = rec.p + rec.normal + random_in_unit_sphere();
-        //point3 target = rec.p + random_in_hemisphere(rec.normal);
-        
-        ray scattered;
-        color attenuation;
-        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth - 1);
-        return color(0,0,0);
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.f);
+	if (world.hit(r, 0.001, infinity, rec))
+	{
+		//point3 target = rec.p + rec.normal + random_unit_vector();
+		//point3 target = rec.p + rec.normal + random_in_unit_sphere();
+		//point3 target = rec.p + random_in_hemisphere(rec.normal);
+
+		ray scattered;
+		color attenuation;
+		if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+			return attenuation * ray_color(scattered, world, depth - 1);
+		return color(0, 0, 0);
+	}
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.f);
 }
 
 hittable_list random_scene() {
-    hittable_list world;
+	hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			auto choose_mat = random_double();
+			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+				shared_ptr<material> sphere_material;
 
-                if (choose_mat < 0.8) {
-                    // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-                else if (choose_mat < 0.95) {
-                    // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-                else {
-                    // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-            }
-        }
-    }
+				if (choose_mat < 0.8) {
+					// diffuse
+					auto albedo = color::random() * color::random();
+					sphere_material = make_shared<lambertian>(albedo);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else if (choose_mat < 0.95) {
+					// metal
+					auto albedo = color::random(0.5, 1);
+					auto fuzz = random_double(0, 0.5);
+					sphere_material = make_shared<metal>(albedo, fuzz);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else {
+					// glass
+					sphere_material = make_shared<dielectric>(1.5);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+			}
+		}
+	}
 
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+	auto material1 = make_shared<dielectric>(1.5);
+	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
-    return world;
+	return world;
 }
 
-int main() 
+void pixel_coloring(int j, int image_width, int image_height, int samples_per_pixel, int max_depth,
+	camera& cam, hittable_list& world, std::vector<std::vector<color>>& all_rows)
+{
+	std::vector<color> temp_vector(image_width);
+	for (int i = 0; i < image_width; ++i)
+	{
+		color pixel_color(0, 0, 0);
+		for (int s = 0; s < samples_per_pixel; ++s)
+		{
+			auto u = (i + random_double()) / (image_width - 1);
+			auto v = (j + random_double()) / (image_height - 1);
+			ray r = cam.get_ray(u, v);
+			pixel_color += ray_color(r, world, max_depth);
+		}
+		temp_vector[i] = pixel_color;
+	}
+	all_rows[image_height - j - 1] = temp_vector;
+}
+
+int main(int argc, char* argv[])
 {
 
-    // Image
-    const auto aspect_ratio = 3.0/2.0;
-    const int image_width = 1200;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 500;
-    const int max_depth = 50;
-    // World
+	// Image
+	const auto aspect_ratio = 3.0 / 2.0;
+	const int image_width = atoi(argv[1]);
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = atoi(argv[2]);
+	const int max_depth = 50;
+	// World
 
-    hittable_list world = random_scene();
+	hittable_list world = random_scene();
 
 
-    point3 lookfrom(13, 2, 3);
-    point3 lookat(0, 0, 0);
-    vec3 vup(0, 1, 0);
-    auto dist_to_focus = (lookfrom - lookat).length();
-    auto aperture = .1;
+	point3 lookfrom(13, 2, 3);
+	point3 lookat(0, 0, 0);
+	vec3 vup(0, 1, 0);
+	auto dist_to_focus = (lookfrom - lookat).length();
+	auto aperture = .1;
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
-    // Render
+	// Render
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+	std::vector<std::vector<color>> all_rows(image_height);
 
-    for (int j = image_height - 1; j >= 0; --j) 
-    {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) 
-        {
-            color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) 
-            {
-                auto u = (i + random_double()) / (image_width - 1);
-                auto v = (j + random_double()) / (image_height - 1);
-                ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
-            }
-            write_color(std::cout, pixel_color, samples_per_pixel);
-        }
-    }
+	auto start = std::chrono::system_clock::now();
 
-    std::cerr << "\nDone.\n";
+	for (int j = image_height - 1; j > 0;)
+	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+		/*
+		for (int i = 0; i < image_width; ++i)
+		{
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s)
+			{
+				auto u = (i + random_double()) / (image_width - 1);
+				auto v = (j + random_double()) / (image_height - 1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world, max_depth);
+			}
+			write_color(std::cout, pixel_color, samples_per_pixel);
+		}
+	}*/
+		std::thread thr0(pixel_coloring,
+			j,
+			image_width, image_height,
+			samples_per_pixel, max_depth,
+			std::ref(cam), std::ref(world), std::ref(all_rows));
+		j--;
+		if (j < 0) j = 0;
+		std::thread thr1(pixel_coloring,
+			j,
+			image_width, image_height,
+			samples_per_pixel, max_depth,
+			std::ref(cam), std::ref(world), std::ref(all_rows));
+		j--;
+		if (j < 0) j = 0;
+		std::thread thr2(pixel_coloring,
+			j,
+			image_width, image_height,
+			samples_per_pixel, max_depth,
+			std::ref(cam), std::ref(world), std::ref(all_rows));
+		j--;
+		if (j < 0) j = 0;
+		std::thread thr3(pixel_coloring,
+			j,
+			image_width, image_height,
+			samples_per_pixel, max_depth,
+			std::ref(cam), std::ref(world), std::ref(all_rows));
+		j--;
+		if (j < 0) j = 0;
+		std::thread thr4(pixel_coloring,
+			j,
+			image_width, image_height,
+			samples_per_pixel, max_depth,
+			std::ref(cam), std::ref(world), std::ref(all_rows));
+		j--;
+		if (j < 0) j = 0;
+		std::thread thr5(pixel_coloring,
+			j,
+			image_width, image_height,
+			samples_per_pixel, max_depth,
+			std::ref(cam), std::ref(world), std::ref(all_rows));
+		j--;
+		if (j < 0) j = 0;
+		std::thread thr6(pixel_coloring,
+			j,
+			image_width, image_height,
+			samples_per_pixel, max_depth,
+			std::ref(cam), std::ref(world), std::ref(all_rows));
+		j--;
+		if (j < 0) j = 0;
+		std::thread thr7(pixel_coloring,
+			j,
+			image_width, image_height,
+			samples_per_pixel, max_depth,
+			std::ref(cam), std::ref(world), std::ref(all_rows));
+		j--;
+		if (j < 0) j = 0;
+		thr0.join();
+		thr1.join();
+		thr2.join();
+		thr3.join();
+		thr4.join();
+		thr5.join();
+		thr6.join();
+		thr7.join();
+	}
+	for (auto row : all_rows)
+	{
+		for (auto &pixel_color : row)
+			write_color(std::cout, pixel_color, samples_per_pixel);
+	}
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> diff = end - start;
+	std::cerr << "\nDone. It took " << diff.count() << " seconds.\n";
 }
